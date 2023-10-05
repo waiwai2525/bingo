9b81e26d2e602bd0323313ed3bdb9248c64a8622
Yuki Iwai <waiwai.cps@gmail.com>
2023-10-05 11:52:08 +0900

    サンプルを削除

--- electron/main.ts
+++ electron/main.ts
@@ -26,11 +26,6 @@ function createWindow() {
     },
   })
 
-  // Test active push message to Renderer-process.
-  win.webContents.on('did-finish-load', () => {
-    win?.webContents.send('main-process-message', (new Date).toLocaleString())
-  })
-
   if (VITE_DEV_SERVER_URL) {
     win.loadURL(VITE_DEV_SERVER_URL)
   } else {
---
+++ src/components/features/bingo/container/BingoContainer.tsx
@@ -0,0 +1,3 @@
+export const BingoContainer = () => {
+    return <div>ビンゴのやつ</div>
+}
