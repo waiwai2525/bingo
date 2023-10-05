785d4bf8f829fb7d01ea132d37d7c0aba8747cfc
Yuki Iwai <waiwai.cps@gmail.com>
2023-10-05 12:01:58 +0900

    react-routerを追加

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,12 @@
+import {Route, Routes} from "react-router-dom";
+
+import {Top} from "./components/pages/Top.tsx";
+
+
 export const App = () => {
-    return <></>
+    return (
+        <Routes>
+            <Route path="/" element={<Top/>}/>
+        </Routes>
+    )
 }
--- src/components/pages/index/Index.tsx
+++
@@ -1,5 +0,0 @@
-import {BingoContainer} from "../../features/bingo/container/BingoContainer.tsx";
-
-export const Index = () => {
-    return <BingoContainer/>
-}
