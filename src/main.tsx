78f548a0ca052aeeb08b8735d70b14ad449cbc2e
waiwai2525 <waiwai.cps@gmail.com>
2023-10-12 23:36:47 +0900

    ルーターをBrowserRouterからHashRouterに変更

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,13 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import {BrowserRouter} from "react-router-dom";
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { HashRouter } from "react-router-dom";
 
-import {App} from "./App.tsx";
+import { App } from "./App.tsx";
 
-
-ReactDOM.createRoot(document.getElementById('root')!).render(
-    <React.StrictMode>
-        <BrowserRouter>
-            <App/>
-        </BrowserRouter>
-    </React.StrictMode>,
-)
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <React.StrictMode>
+    <HashRouter>
+      <App />
+    </HashRouter>
+  </React.StrictMode>
+);
